136046c6eb05a9a78ba6bad3c4e909b7e9aba6e2
kostykrasil <163845822+kostykrasil@users.noreply.github.com>
2026-04-18 17:59:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,87 +1,82 @@
-// Copyright 2025 NNTU-CS
+// Copyright 2021 NNTU-CS
 #include "alg.h"
-
 #include <cctype>
 #include <sstream>
 #include <string>
-
 #include "tstack.h"
 
 int priority(char op) {
-    if (op == '+' || op == '-') return 1;
-    if (op == '*' || op == '/') return 2;
-    return 0;
+  if (op == '+' || op == '-') return 1;
+  if (op == '*' || op == '/') return 2;
+  return 0;
 }
 
 std::string infx2pstfx(const std::string& inf) {
-    TStack<char, 100> st;
-    std::string result;
-    size_t i = 0;
-    int len = inf.length();
+  TStack<char, 100> st;
+  std::string result;
+  size_t i = 0;
+  int len = inf.length();
 
-    while (i < len) {
-        char ch = inf[i];
-        if (isdigit(ch)) {
-            while (i < len && isdigit(inf[i])) {
-                result += inf[i];
-                ++i;
-            }
-            result += ' ';
-            continue;
-        } else if (ch == '(') {
-            st.push(ch);
-        } else if (ch == ')') {
-            while (!st.isEmpty() && st.top() != '(') { 
-                result += st.pop();
-                result += ' ';
-            }
-            if (!st.isEmpty() && st.top() == '(') {
-                st.pop();
-            }
-        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-            while (!st.isEmpty() && st.top() != '(' &&
-                   priority(st.top()) >= priority(ch)) {
-                result += st.pop();
-                result += ' ';
-            }
-            st.push(ch);
-        }
+  while (i < len) {
+    char ch = inf[i];
+    if (isdigit(ch)) {
+      while (i < len && isdigit(inf[i])) {
+        result += inf[i];
         ++i;
-    }
-
-    while (!st.isEmpty()) {
+      }
+      result += ' ';
+      continue;
+    } else if (ch == '(') {
+      st.push(ch);
+    } else if (ch == ')') {
+      while (!st.isEmpty() && st.get() != '(') {
         result += st.pop();
         result += ' ';
+      }
+      if (!st.isEmpty() && st.get() == '(') {
+        st.pop();
+      }
+    } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+      while (!st.isEmpty() && st.get() != '(' &&
+             priority(st.get()) >= priority(ch)) {
+        result += st.pop();
+        result += ' ';
+      }
+      st.push(ch);
     }
-
-    if (!result.empty() && result.back() == ' ') {
-        result.pop_back();
-    }
-
-    return result;
+    ++i;
+  }
+  while (!st.isEmpty()) {
+    result += st.pop();
+    result += ' ';
+  }
+  if (!result.empty() && result.back() == ' ') {
+    result.pop_back();
+  }
+  return result;
 }
 
 int eval(const std::string& post) {
-    TStack<int, 100> st;
-    std::istringstream iss(post);
-    std::string token;
+  TStack<int, 100> st;
+  std::istringstream iss(post);
+  std::string token;
 
-    while (iss >> token) {
-        if (isdigit(token[0]) || (token.size() > 1 && token[0] == '-')) {
-            st.push(std::stoi(token));
-        } else {
-            int right = st.pop();
-            int left = st.pop();
-            int res = 0;
-            switch (token[0]) {
-                case '+': res = left + right; break;
-                case '-': res = left - right; break;
-                case '*': res = left * right; break;
-                case '/': res = left / right; break;
-                default: break;
-            }
-            st.push(res);
-        }
+  while (iss >> token) {
+    if (isdigit(token[0]) || (token.size() > 1 && token[0] == '-')) {
+      st.push(std::stoi(token));
+    } else {
+      int right = st.pop();
+      int left = st.pop();
+      int res = 0;
+      switch (token[0]) {
+        case '+': res = left + right; break;
+        case '-': res = left - right; break;
+        case '*': res = left * right; break;
+        case '/': res = left / right; break;
+        default: break;
+      }
+      st.push(res);
     }
-    return st.pop();
+  }
+  return st.pop();
 }
