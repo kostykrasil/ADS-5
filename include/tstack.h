f8629e63d833e74147cb7360e95ac48bbaa490be
kostykrasil <163845822+kostykrasil@users.noreply.github.com>
2026-04-18 17:58:26 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,36 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T data[size];
+  int topIndex;
+
+ public:
+  TStack() : topIndex(-1) {}
+
+  void push(T value) {
+    if (topIndex < size - 1) {
+      data[++topIndex] = value;
+    }
+  }
+
+  T pop() {
+    if (topIndex >= 0) {
+      return data[topIndex--];
+    }
+    return T();
+  }
+
+  T get() const {
+    if (topIndex >= 0) {
+      return data[topIndex];
+    }
+    return T();
+  }
+
+  bool isEmpty() const {
+    return topIndex == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
